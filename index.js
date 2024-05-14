6300e9d0abd7b8b7822f13f30f11c5b78dc049aa
ahmedkhafaga621 <ahmedkhafaga621@icloud.com>
2024-05-14 22:50:57 +0300

    upload

---
+++ index.js
@@ -0,0 +1,22 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var diceImage = "dice" + randomNumber1 + ".png";
+var randomImage = "images/" + diceImage ; 
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImage);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var diceImage2 = "dice" + randomNumber2 + ".png";
+var randomImage2 = "images/" + diceImage2 ; 
+ document.querySelectorAll("img")[1].setAttribute("src",randomImage2);
+
+
+ if (randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML = "player 1 win "
+}else if (randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML = "player 2 win "
+} else{
+    document.querySelector("h1").innerHTML = "Draw  "
+
+ }
+
